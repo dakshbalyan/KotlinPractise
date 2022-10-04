d56d66f86d1b993526a217cdf14e184b98965cb1
dakshbalyan <daksh.balyan@gmail.com>
2022-10-04 23:10:39 +0530

    added solved ques name

--- src/main/kotlin/org/personal/practise/leetcode/trees/EasyTree.kt
+++ src/main/kotlin/org/personal/practise/leetcode/trees/EasyTree.kt
@@ -3,6 +3,8 @@ package org.personal.practise.leetcode.trees
 import org.personal.practise.leetcode.utils.TreeNode
 
 class EasyTree {
+
+    // 112. Path Sum
     fun hasPathSum(root: TreeNode?, targetSum: Int): Boolean {
         if (root == null) return false
         if (root.left == null && root.right == null) {
