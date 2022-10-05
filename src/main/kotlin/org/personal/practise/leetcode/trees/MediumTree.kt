15cae05b7d9f5d584d1f821d8efc9c6386ea865f
dakshbalyan <daksh.balyan@gmail.com>
2022-10-05 15:54:31 +0530

    Solved 623. Add One Row to Tree

--- src/main/kotlin/org/personal/practise/leetcode/trees/MediumTree.kt
+++ src/main/kotlin/org/personal/practise/leetcode/trees/MediumTree.kt
@@ -1,6 +1,8 @@
 package org.personal.practise.leetcode.trees
 
 import org.personal.practise.leetcode.utils.TreeNode
+import java.util.LinkedList
+import java.util.Queue
 
 class MediumTree {
 
@@ -25,6 +27,71 @@ class MediumTree {
         pathSum(root.left, targetSum - root.`val`, ans, currPath)
         pathSum(root.right, targetSum - root.`val`, ans, currPath)
         currPath.removeLast()
+    }
+
+    // 623. Add One Row to Tree
+    fun addOneRow(root: TreeNode?, `val`: Int, depth: Int): TreeNode? {
+        val new = addRowIteratively(root, `val`, depth)
+        return addRowRecursively(root, `val`, depth)
+    }
+
+    private fun addRowRecursively(root: TreeNode?, value: Int, depth: Int): TreeNode? {
+        // Base case
+        if (root == null) return TreeNode(value)
+        if (depth == 1) {
+            val newRoot = TreeNode(value)
+            newRoot.left = root
+            return newRoot
+        }
+
+        // Induction step
+        if (depth == 2) {
+            val tmpLeft = TreeNode(value)
+            val tmpRight = TreeNode(value)
+            tmpLeft.left = root.left
+            tmpRight.right = root.right
+            root.left = tmpLeft
+            root.right = tmpRight
+        }
+
+        // Recursive step
+        addRowRecursively(root.left, value, depth.minus(1))
+        addRowRecursively(root.right, value, depth.minus(1))
+        return root
+    }
+
+    private fun addRowIteratively(root: TreeNode?, value: Int, depth: Int): TreeNode? {
+        if (root == null) return TreeNode(value)
+        if (depth == 1) {
+            val newRoot = TreeNode(value)
+            newRoot.left = root
+            return newRoot
+        }
+
+        var level = 1
+        val queue: Queue<TreeNode> = LinkedList<TreeNode>()
+        queue.add(root)
+
+        while (queue.size != 0 && level < depth) {
+            level++
+            val size = queue.size
+            for (i in 0 until size) {
+                val currNode = queue.remove()
+                if (level == depth) {
+                    val tmpLeft = TreeNode(value)
+                    val tmpRight = TreeNode(value)
+                    tmpLeft.left = currNode.left
+                    tmpRight.right = currNode.right
+                    currNode.left = tmpLeft
+                    currNode.right = tmpRight
+                } else {
+                    if (currNode.left != null) queue.add(currNode.left)
+                    if (currNode.right != null) queue.add(currNode.right)
+                }
+            }
+        }
+
+        return root
     }
 
 }
